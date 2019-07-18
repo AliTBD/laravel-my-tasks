252fb731cbb8989e3e8f75bcaed7eac1e242bb77
Ali Almalki <dev.alialmalki@gmail.com>
2019-07-18 14:04:23 +0300

    fix the navbar

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -29,15 +29,17 @@
                             </li>
                         @endif
                     @else
+                        <li class="nav-item">
+                            <a class="nav-link text-dark" href="/projects">My project</a>
+                        </li>
                         <li class="nav-item dropdown">
+
                             <a id="navbarDropdown" class="nav-link dropdown-toggle text-dark" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }}
                                 <span class="caret"></span>
                             </a>
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-
-                                <a class="dropdown-item text-dark" href="/projects">My project</a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
 
                                 <a class="dropdown-item text-dark" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
